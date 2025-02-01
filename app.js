26815146ba876ae1a19362236494db6cf54042b4
RayoPonce <rayoponce2@gmail.com>
2025-02-01 00:39:10 -0600

    Se crea un array para almacenar los nombres

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+     
+    let amigoAgregado = document.getElementById('amigo').value;
+    console.log(amigoAgregado);
+
+    if (amigoAgregado = ''){
+        alert("Por favor, inserte un nombre");
+    }else{
+        amigos.push(amigoAgregado);
+    }
+}
